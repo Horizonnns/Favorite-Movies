c061ddb92890cf72aacf0af2b70971a62fe16cb4
Kesedy <kosimjon.kosimov>
2023-04-26 11:25:47 +0500

    Changed info to date

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -8,16 +8,15 @@ export const useMovieStore = defineStore('movieStore', {
 				original_title: 'Spider-Man',
 				overview:
 					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, ducimus unde quia nisi expedita laudantium autem cumque ex magnam esse impedit fugiat, quasi corporis modi atque, inventore sint odit obcaecati!',
-				poster_path: '/static.wikia.nocookie.net/spidermanps4/images/d/d4/Marvel%27s_Spider-Man_front_cover_%28US%29.png/revision/latest/scale-to-width-down/1200?cb=20201003182432',
+				poster_path: '/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg',
 				release_date: '2002-05-01',
 				isWatched: false,
 			},
 			{
 				id: 2,
 				original_title: 'The Batman',
-				overview:
-					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, ducimus unde quia nisi expedita laudantium autem cumque ex magnam esse impedit fugiat, quasi corporis modi atque, inventore sint odit obcaecati!',
-				poster_path: '/static.wikia.nocookie.net/spidermanps4/images/d/d4/Marvel%27s_Spider-Man_front_cover_%28US%29.png/revision/latest/scale-to-width-down/1200?cb=20201003182432',
+				overview: 'In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.',
+				poster_path: '/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg',
 				release_date: '2022-03-01',
 				isWatched: false,
 			},
